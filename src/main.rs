96f66051618e665939bab5e25c7881996b041d6e
Tim Harding <tim@timharding.co>
2022-03-05 15:43:22 -0800

    Blank white image

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
 use image::{ImageBuffer};
 
 fn main() -> anyhow::Result<()> {
-    let d = 512;
-    let img = ImageBuffer::from_fn(d, d, |x,y| {
-        let r = (d / 2) as f32;
-        let x = x as f32 - r;
-        let y = y as f32 - r;
-        let r = (x * x + y * y) / (r*r);
-        image::Rgb([r; 3])
-    });
+    let img = ImageBuffer::from_pixel(512, 512, image::Rgb([1f32;3]));
     img.save("render.exr")?;
     Ok(())
 }
