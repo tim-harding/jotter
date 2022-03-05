9dd32367cbc7b66e305001d11fff3bfd815b7269
Tim Harding <tim@timharding.co>
2022-03-05 15:31:23 -0800

    Writing an EXR

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use image::{ImageBuffer};
+
+fn main() -> anyhow::Result<()> {
+    let img = ImageBuffer::from_fn(512, 512, |x,_| {
+        if x % 2 == 0 {
+            image::Rgb([0f32; 3])
+        } else {
+            image::Rgb([1f32; 3])
+        }
+    });
+    img.save("render.exr")?;
+    Ok(())
 }
