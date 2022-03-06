a6ae8eef43c87eed3ce5c427578d67b6c775bc87
Tim Harding <tim@timharding.co>
2022-03-05 16:20:39 -0800

    Dotting across image

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 use image::{ImageBuffer};
 
 fn main() -> anyhow::Result<()> {
-    let img = ImageBuffer::from_pixel(512, 512, image::Rgb([1f32;3]));
+    let mut img = ImageBuffer::from_pixel(512, 512, image::Rgb([1f32;3]));
+    for p in 0..5 {
+        let gap = 512 / 5;
+        let center = p * gap;
+        let r = 10;
+        let start = if center < r { 0 } else { center - r };
+        let end = if center > 512 - r { 512 } else { center + r };
+        for x in start..end {
+            img.put_pixel(x, 256, image::Rgb([0f32; 3]));
+        }
+    }
     img.save("render.exr")?;
     Ok(())
 }
