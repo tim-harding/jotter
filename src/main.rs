8419313a31d1c5d4f4e57a7e28efd26345645a74
Tim Harding <tim@timharding.co>
2022-03-10 17:53:21 -0800

    Falloff around points

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,11 @@ impl Point {
     }
 }
 
+const R: i32 = 5;
+const W: i32 = 512;
+
 fn main() -> BoxResult<()> {
-    let r = 5;
-    let w = 512;
-    let dim = 512 + 2 * r;
+    let dim = (W + 2 * R) as u32;
     let mut img = ImageBuffer::from_pixel(dim, dim, image::Rgb([1f32; 3]));
     let points = [
         Point::new(0.25, 0.25),
@@ -25,13 +26,16 @@ fn main() -> BoxResult<()> {
         Point::new(0.75, 0.75),
         Point::new(0.5, 0.5),
     ];
-    let wf = w as f32;
+    let wf = W as f32;
     for point in points.iter() {
-        let cx = (point.x * wf) as u32;
-        let cy = (point.y * wf) as u32;
-        for x in (r + cx)..(3 * r + cx) {
-            for y in (r + cy)..(3*r + cy) {
-                img.put_pixel(x, y, image::Rgb([0f32; 3]));
+        let cx = (point.x * wf) as i32;
+        let cy = (point.y * wf) as i32;
+        for x in (cx - R as i32)..(cx + R as i32) {
+            for y in (cy - R as i32)..(cy + R as i32) {
+                let xf = (x - cx) as f32;
+                let yf = (y - cy) as f32;
+                let r = (xf * xf + yf * yf) / R as f32;
+                img.put_pixel((x + R) as u32, (y + R) as u32, image::Rgb([r; 3]));
             }
         }
     }
