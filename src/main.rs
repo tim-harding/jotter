78dbf67aa522298b67b8d675da434c8e862d89fa
Tim Harding <tim@timharding.co>
2022-03-10 23:10:18 -0800

    Fixed graphic

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use image::ImageBuffer;
 
 type BoxResult<T> = Result<T, Box<dyn std::error::Error>>;
 
-const RADIUS: i32 = 2;
+const RADIUS: i32 = 3;
 const RADIUS_F: f32 = RADIUS as f32;
 
 fn main() -> BoxResult<()> {
@@ -13,28 +13,25 @@ fn main() -> BoxResult<()> {
         dims.1 + RADIUS as u32 * 2 + 2,
         image::Rgb([1f32; 3]),
     );
-    let count = 1 << 19;
+    let count = 1 << 10;
     for r_base in 0..count {
         let r = r_base as f32 / count as f32 * 4.0;
         let mut x = 0.5;
         for _ in 0..1024 {
             x = r * x * (1.0 - x);
-        }
-        let r = r / 4.0;
-        let cr = (r * dims_f.0) as i32;
-        let cx = (x * dims_f.1) as i32;
-        for r in (cr - RADIUS - 1)..(cr + RADIUS + 1) {
-            for x in (cx - RADIUS - 1)..(cx + RADIUS + 1) {
-                let xf = r as f32 - cr as f32;
-                let yf = x as f32 - cx as f32;
-                let t = (xf * xf + yf * yf) / RADIUS_F / RADIUS_F;
-                let t = t.min(1.0);
-                // let t = 3.0 * t * t - 2.0 * t * t * t;
-                let t = 1.0 - (1.0 - t) * 0.5;
-                let r = (r + RADIUS + 1) as u32;
-                let x = (x + RADIUS + 1) as u32;
-                let a = img.get_pixel(r, x).0[0];
-                img.put_pixel(r, x, image::Rgb([a * t; 3]));
+            let r = r / 4.0;
+            for r_offset in -RADIUS..RADIUS {
+                for x_offset in -RADIUS..RADIUS {
+                    // Still not quite right
+                    let rf = r_offset as f32;
+                    let xf = x_offset as f32;
+                    let t = ((rf * rf + xf * xf) / RADIUS_F / RADIUS_F).min(1.0);
+                    let t = 1.0 - (1.0 - t) * 0.25;
+                    let r = ((r * dims_f.0) as i32 + r_offset + RADIUS + 1) as u32;
+                    let x = ((x * dims_f.1) as i32 + x_offset + RADIUS + 1) as u32;
+                    let a = img.get_pixel(r, x).0[0];
+                    img.put_pixel(r, x, image::Rgb([a * t; 3]));
+                }
             }
         }
     }
