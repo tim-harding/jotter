d1201fd151f97e3dc8c7208efe604249b203ca1e
Tim Harding <tim@timharding.co>
2022-03-10 22:37:12 -0800

    First actual logistic render

--- src/main.rs
+++ src/main.rs
@@ -2,45 +2,38 @@ use image::ImageBuffer;
 
 type BoxResult<T> = Result<T, Box<dyn std::error::Error>>;
 
-struct Point {
-    x: f32,
-    y: f32,
-}
-
-impl Point {
-    const fn new(x: f32, y: f32) -> Self {
-        Self { x, y }
-    }
-}
-
-const R: i32 = 5;
-const W: i32 = 512;
+const RADIUS: i32 = 2;
+const RADIUS_F: f32 = RADIUS as f32;
 
 fn main() -> BoxResult<()> {
-    let dim = (W + 2 * R) as u32;
-    let mut img = ImageBuffer::from_pixel(dim, dim, image::Rgb([1f32; 3]));
-    let wf = W as f32;
-    let mut x = 1489;
-    let mut y = 653;
-    for _ in 0..4096 {
-        x = (1489 * x) % 503;
-        y = (653 * y) % 509;
-        let point = Point::new(x as f32 / wf, y as f32 / wf);
-        let cx = (point.x * wf) as i32;
-        let cy = (point.y * wf) as i32;
-        for x in (cx - R)..(cx + R) {
-            for y in (cy - R)..(cy + R) {
-                let xf = x as f32 - cx as f32;
-                let yf = y as f32 - cy as f32;
-                let r = (xf * xf + yf * yf) / (R as f32) / (R as f32);
-                let r = r.min(1.0);
-                let r = 1.0 - (1.0 - r) * (1.0 - r);
-                let r = 3.0 * r * r - 2.0 * r * r * r;
-                let r = 1.0 - (1.0 - r) * 0.75;
-                let x = (x + R) as u32;
-                let y = (y + R) as u32;
-                let a = img.get_pixel(x, y).0[0];
-                img.put_pixel(x, y, image::Rgb([a * r; 3]));
+    let dims = (4096u32, 1024u32);
+    let dims_f = (dims.0 as f32, dims.1 as f32);
+    let mut img = ImageBuffer::from_pixel(
+        dims.0 + RADIUS as u32 * 2,
+        dims.1 + RADIUS as u32 * 2,
+        image::Rgb([1f32; 3]),
+    );
+    for r_base in 0..4096 {
+        let r = r_base as f32 / 4096.0 * 4.0;
+        let mut x = 0.5;
+        for _ in 0..16 {
+            x = r * x * (1.0 - x);
+        }
+        let r = r / 4.0;
+        let cr = (r * dims_f.0) as i32;
+        let cx = (x * dims_f.1) as i32;
+        for r in (cr - RADIUS)..(cr + RADIUS) {
+            for x in (cx - RADIUS)..(cx + RADIUS) {
+                let xf = r as f32 - cr as f32;
+                let yf = x as f32 - cx as f32;
+                let t = (xf * xf + yf * yf) / RADIUS_F / RADIUS_F;
+                let t = t.min(1.0);
+                let t = 3.0 * t * t - 2.0 * t * t * t;
+                let t = 1.0 - (1.0 - t) * 0.5;
+                let r = (r + RADIUS) as u32;
+                let x = (x + RADIUS) as u32;
+                let a = img.get_pixel(r, x).0[0];
+                img.put_pixel(r, x, image::Rgb([a * t; 3]));
             }
         }
     }
