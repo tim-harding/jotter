6fc47c679dbcfc4ff2ded3d919677202a26853cf
Tim Harding <tim@timharding.co>
2022-03-10 18:45:53 -0800

    Drawing soft points

--- src/main.rs
+++ src/main.rs
@@ -20,20 +20,27 @@ fn main() -> BoxResult<()> {
     let dim = (W + 2 * R) as u32;
     let mut img = ImageBuffer::from_pixel(dim, dim, image::Rgb([1f32; 3]));
     let wf = W as f32;
-    let mut x = 7;
-    let mut y = 11;
+    let mut x = 1489;
+    let mut y = 653;
     for _ in 0..4096 {
-        x = (7 * x) % 503;
-        y = (11 * y) % 509;
+        x = (1489 * x) % 503;
+        y = (653 * y) % 509;
         let point = Point::new(x as f32 / wf, y as f32 / wf);
         let cx = (point.x * wf) as i32;
         let cy = (point.y * wf) as i32;
         for x in (cx - R)..(cx + R) {
             for y in (cy - R)..(cy + R) {
-                let xf = (x - cx) as f32;
-                let yf = (y - cy) as f32;
+                let xf = x as f32 - cx as f32;
+                let yf = y as f32 - cy as f32;
                 let r = (xf * xf + yf * yf) / (R as f32) / (R as f32);
-                img.put_pixel((x + R) as u32, (y + R) as u32, image::Rgb([r; 3]));
+                let r = r.min(1.0);
+                let r = 1.0 - (1.0 - r) * (1.0 - r);
+                let r = 3.0 * r * r - 2.0 * r * r * r;
+                let r = 1.0 - (1.0 - r) * 0.75;
+                let x = (x + R) as u32;
+                let y = (y + R) as u32;
+                let a = img.get_pixel(x, y).0[0];
+                img.put_pixel(x, y, image::Rgb([a * r; 3]));
             }
         }
     }
