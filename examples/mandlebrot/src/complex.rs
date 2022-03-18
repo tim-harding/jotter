8cfc37e06de4f1be097fa97783ff935e0d255073
Tim Harding <tim@timharding.co>
2022-03-18 16:47:31 -0700

    Displaying Mandlebrot

---
+++ examples/mandlebrot/src/complex.rs
@@ -0,0 +1,57 @@
+use jotter::Vector;
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct Complex {
+    real: f32,
+    imaginary: f32,
+}
+
+impl Complex {
+    pub fn new(real: f32, imaginary: f32) -> Self {
+        Self { real, imaginary }
+    }
+
+    pub fn abs(&self) -> f32 {
+        self.real * self.real + self.imaginary * self.imaginary
+    }
+}
+
+impl std::ops::Add<Complex> for Complex {
+    type Output = Self;
+
+    fn add(self, rhs: Complex) -> Self {
+        Self {
+            real: self.real + rhs.real,
+            imaginary: self.imaginary + rhs.imaginary,
+        }
+    }
+}
+
+impl std::ops::Mul<Complex> for Complex {
+    type Output = Self;
+
+    fn mul(self, rhs: Complex) -> Self {
+        Self {
+            real: self.real * rhs.real - self.imaginary * rhs.imaginary,
+            imaginary: self.real * rhs.imaginary + self.imaginary * rhs.real,
+        }
+    }
+}
+
+impl From<Complex> for Vector {
+    fn from(complex: Complex) -> Self {
+        Self::new(complex.real, complex.imaginary)
+    }
+}
+
+impl From<Vector> for Complex {
+    fn from(vector: Vector) -> Self {
+        Self::new(vector.x, vector.y)
+    }
+}
+
+impl From<f32> for Complex {
+    fn from(n: f32) -> Self {
+        Self::new(n, 0.0)
+    }
+}
--- examples/mandlebrot/src/main.rs
+++ examples/mandlebrot/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world")
+use complex::Complex;
+use jotter::{Image, Rect, Vector};
+
+mod complex;
+
+fn main() -> Result<(), jotter::Error> {
+    let mut image = Image::new(1024, 1024, 0.0);
+    let rect = Rect::with_bounds(-1.0, 1.0, -1.0, 1.0);
+    for y in 0..image.height() {
+        for x in 0..image.width() {
+            let view = &Vector::new(
+                x as f32 / image.width() as f32,
+                y as f32 / image.height() as f32,
+            );
+            let point: Complex = rect.to_world(view).into();
+            let mut z: Complex = 0.0.into();
+            let mut n = 1;
+            while n < 64 && z.abs() <= 2.0 {
+                z = z * z + point;
+                n += 1;
+            }
+            image.set(x, y, 1.0 / n as f32);
+        }
+    }
+    image.save("mandelbrot.exr")?;
+    Ok(())
 }
