b953042c2f2e0635e07e663d13b31b11fa7dda04
Tim Harding <tim@timharding.co>
2022-03-19 16:06:47 -0700

    Denser Ulam spiral

--- examples/ulam_spiral/src/main.rs
+++ examples/ulam_spiral/src/main.rs
@@ -1,20 +1,20 @@
 use jotter::{Image, Rect, Vector, View};
-use std::f32::consts::TAU;
+use std::f64::consts::TAU;
 
 #[derive(Debug, Default, Clone, Copy)]
 struct ArchimedesSpiral {
-    step: usize,
+    step: u64,
 }
 
 impl ArchimedesSpiral {
     pub fn next(&mut self) -> Vector {
-        let t = self.step as f32;
+        let t = self.step as f64;
         let t = t.sqrt();
         let trig_parameter = TAU * t;
         let x = t * trig_parameter.cos();
         let y = t * trig_parameter.sin();
         self.step += 1;
-        Vector::new(x, y)
+        Vector::new(x as f32, y as f32)
     }
 }
 
@@ -44,7 +44,7 @@ impl Bits {
 
 fn main() -> Result<(), jotter::Error> {
     const DIM: usize = 4096;
-    const SQRT_PRIME_COUNT: usize = 1 << 12;
+    const SQRT_PRIME_COUNT: usize = 1 << 14;
     const SQRT_PRIME_COUNT_F: f32 = SQRT_PRIME_COUNT as f32;
     let mut image = Image::new(DIM, DIM, 1.0);
     let rect = Rect::with_bounds(
@@ -56,11 +56,12 @@ fn main() -> Result<(), jotter::Error> {
     let mut view = View::new(&mut image, rect);
     let mut spiral = ArchimedesSpiral::default();
     const ODDS_COUNT: usize = SQRT_PRIME_COUNT * SQRT_PRIME_COUNT / 2;
+    const OPACITY: f32 = 0.25;
     let mut is_composite_vec = Bits::new(ODDS_COUNT);
     {
         spiral.next();
         let two = spiral.next();
-        view.splat(two, 1.0);
+        view.splat(two, OPACITY);
     }
     for i in 1..ODDS_COUNT {
         let is_prime = !is_composite_vec.get(i);
@@ -73,7 +74,7 @@ fn main() -> Result<(), jotter::Error> {
                 is_composite_vec.set(mask);
                 mask += number;
             }
-            view.splat(p, 1.0);
+            view.splat(p, OPACITY);
         }
     }
     image.save("ulam_spiral.exr")?;
