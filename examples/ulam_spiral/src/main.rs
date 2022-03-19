617bdeff899513b0842bd6959dc8913e55bf0fe0
Tim Harding <tim@timharding.co>
2022-03-19 16:03:32 -0700

    Homegrown bitvec

--- examples/ulam_spiral/src/main.rs
+++ examples/ulam_spiral/src/main.rs
@@ -1,4 +1,3 @@
-use bitvec::prelude::*;
 use jotter::{Image, Rect, Vector, View};
 use std::f32::consts::TAU;
 
@@ -19,6 +18,30 @@ impl ArchimedesSpiral {
     }
 }
 
+struct Bits {
+    data: Vec<u64>,
+}
+
+impl Bits {
+    pub fn new(count: usize) -> Self {
+        Self {
+            data: vec![0; count / 64 + 1],
+        }
+    }
+
+    pub fn set(&mut self, i: usize) {
+        let n = i / 64;
+        let bit = i % 64;
+        self.data[n] |= 1 << bit;
+    }
+
+    pub fn get(&self, i: usize) -> bool {
+        let n = i / 64;
+        let bit = i % 64;
+        (self.data[n] >> bit) & 1 == 1
+    }
+}
+
 fn main() -> Result<(), jotter::Error> {
     const DIM: usize = 4096;
     const SQRT_PRIME_COUNT: usize = 1 << 12;
@@ -33,21 +56,21 @@ fn main() -> Result<(), jotter::Error> {
     let mut view = View::new(&mut image, rect);
     let mut spiral = ArchimedesSpiral::default();
     const ODDS_COUNT: usize = SQRT_PRIME_COUNT * SQRT_PRIME_COUNT / 2;
-    let mut is_composite_vec: BitArr!(for ODDS_COUNT) = BitArray::ZERO;
+    let mut is_composite_vec = Bits::new(ODDS_COUNT);
     {
         spiral.next();
         let two = spiral.next();
         view.splat(two, 1.0);
     }
     for i in 1..ODDS_COUNT {
-        let is_prime = !is_composite_vec[i];
+        let is_prime = !is_composite_vec.get(i);
         let p = spiral.next();
         spiral.next();
         if is_prime {
             let number = i * 2 + 1;
             let mut mask = i;
             while mask < ODDS_COUNT {
-                is_composite_vec.set(mask, true);
+                is_composite_vec.set(mask);
                 mask += number;
             }
             view.splat(p, 1.0);
